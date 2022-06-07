d9764f4564f5424038cfdfe07475edd7c078f93a
Anton Petrov <marblemonolit@gmail.com>
2022-06-07 12:36:56 +0300

    nvim-tree config update, treesitter linting.

--- lua/plugins/nvim-tree.lua
+++ lua/plugins/nvim-tree.lua
@@ -2,21 +2,34 @@ local nvim_tree_config = require("nvim-tree.config")
 local tree_cb = nvim_tree_config.nvim_tree_callback
 
 --   ﭜ ﭧ     
-vim.g.nvim_tree_icons = {
-    git = {
-        unstaged = "",
-        staged = "",
-        unmerged = "",
-        renamed = "ﰲ",
-        deleted = "",
-        untracked = "",
-        ignored = "",
-    },
-}
-
 require("nvim-tree").setup {
-    auto_close = true,
+    -- cwd - current working directory
+    update_cwd = true,
     open_on_tab = false,
+    auto_reload_on_write = true,
+    view = {
+        mappings = {
+            custom_only = false,
+            list = {
+                { key = "v", cb = tree_cb("vsplit") },
+            }
+        }
+    },
+    renderer = {
+        icons = {
+            glyphs = {
+                git = {
+                    unstaged = "",
+                    staged = "",
+                    unmerged = "",
+                    renamed = "ﰲ",
+                    deleted = "",
+                    untracked = "",
+                    ignored = "",
+                }
+            }
+        }
+    },
     diagnostics = {
         enable = true,
         icons = {
@@ -28,16 +41,19 @@ require("nvim-tree").setup {
     },
     git = {
         enable = true,
+        ignore = true,
+        internal = 400,
     },
     update_focused_file = {
-        enable = true
+        enable = true,
+        update_cwd = true,
     },
-    view = {
-        mappings = {
-            custom_only = false,
-            list = {
-                { key = "v", cb = tree_cb("vsplit") },
-            }
-        }
-    }
+    hijack_directories = {
+        enable = true,
+        auto_open = true,
+    },
+    filesystem_watchers = {
+        enable = true,
+        interval = 300,
+    },
 }
--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,11 +1,10 @@
-require("nvim-treesitter.configs").setup({
+require("nvim-treesitter.configs").setup {
     highlight = { enable = true },
-    context_commentstring = { enable = true, enable_autocmd = false},
+    context_commentstring = { enable = true, enable_autocmd = false },
     rainbow = { enable = true, extended_mode = true, max_file_lines = nil },
     autotag = { enable = true },
     indent = { enable = true },
     ensure_installed = {
         "html", "scss", "javascript", "lua", "python", "toml", "yaml", "json"
     }
-})
-
+}
