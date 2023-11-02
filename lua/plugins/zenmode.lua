86ab12e3163de9ab79604bdbe9b2651554e3449a
Anton <antion@Antons-MacBook-Pro.local>
2023-11-02 17:40:41 +0400

    Zen mode configuration adjustments

--- lua/plugins/zenmode.lua
+++ lua/plugins/zenmode.lua
@@ -1,24 +1,24 @@
 local zenmode_status_ok, zenmode = pcall(require, "zen-mode")
 if not zenmode_status_ok then
-  return
+	return
 end
 
-zenmode.setup {
-  window = {
-    backdrop = 0.95,
-    width = 120,
-    height = 1,
-    options = {
-      -- signcolumn = "no", -- disable signcolumn
-      -- number = false, -- disable number column
-      -- relativenumber = false, -- disable relative numbers
-      -- cursorline = false, -- disable cursorline
-      -- cursorcolumn = false, -- disable cursor column
-      -- foldcolumn = "0", -- disable fold column
-      -- list = false, -- disable whitespace characters
-    },
-  },
-  plugins = {
-    twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
-  }
-}
+zenmode.setup({
+	window = {
+		backdrop = 1,
+		width = 120,
+		height = 1,
+		options = {
+			-- signcolumn = "no", -- disable signcolumn
+			-- number = false, -- disable number column
+			-- relativenumber = false, -- disable relative numbers
+			-- cursorline = false, -- disable cursorline
+			-- cursorcolumn = false, -- disable cursor column
+			-- foldcolumn = "0", -- disable fold column
+			-- list = false, -- disable whitespace characters
+		},
+	},
+	plugins = {
+		twilight = { enabled = true }, -- enable to start Twilight when zen mode opens
+	},
+})
