cbf37309998cb1d1d62e22b4aeec7991529b099b
Anton <antion@Antons-MacBook-Pro.local>
2023-11-04 17:45:48 +0400

    NerdFont icons update for alpha configuration

--- lua/plugins/alpha.lua
+++ lua/plugins/alpha.lua
@@ -1,14 +1,12 @@
 local dashboard = require("alpha.themes.dashboard")
 
 dashboard.section.buttons.val = {
-  dashboard.button("e", "  New file", "<cmd>ene <CR>"),
-  dashboard.button("f", "  Find file", ":Telescope find_files <CR>"),
-  dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
-  dashboard.button("r", "  Recently opened files", ":Telescope oldfiles <CR>"),
-  dashboard.button("t", "  Find text", ":Telescope live_grep <CR>"),
-  dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
+	dashboard.button("e", "  New file", "<cmd>ene <CR>"),
+	dashboard.button("f", "󰍉  Find file", ":Telescope find_files <CR>"),
+	dashboard.button("p", "  Find project", ":Telescope projects <CR>"),
+	dashboard.button("r", "󰈢  Recently opened files", ":Telescope oldfiles <CR>"),
+	dashboard.button("t", "󰊄  Find text", ":Telescope live_grep <CR>"),
+	dashboard.button("q", "  Quit Neovim", ":qa<CR>"),
 }
 
-require("alpha").setup(
-  dashboard.opts
-)
+require("alpha").setup(dashboard.opts)
