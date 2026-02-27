1304146beb94d0b825a7d9aba262d984ef5d38c3
Anton Petrov <mrprfrm@gmail.com>
2026-02-27 12:12:47 +0400

    nvim diff options adjustments

--- nvim/.config/nvim/lua/config/options.lua
+++ nvim/.config/nvim/lua/config/options.lua
@@ -35,33 +35,36 @@ vim.opt.showtabline = 2
 vim.opt.splitbelow = true
 vim.opt.splitright = true
 
+-- diff options
+vim.opt.diffopt:append("vertical")
+
 -- vim.opt.cmdheight = 2
 
 local signs = {
-  { name = "DiagnosticSignError", text = "󰅙" },
-  { name = "DiagnosticSignWarn", text = "" },
-  { name = "DiagnosticSignHint", text = "󰌵" },
-  { name = "DiagnosticSignInfo", text = "" },
+	{ name = "DiagnosticSignError", text = "󰅙" },
+	{ name = "DiagnosticSignWarn", text = "" },
+	{ name = "DiagnosticSignHint", text = "󰌵" },
+	{ name = "DiagnosticSignInfo", text = "" },
 }
 
 for _, sign in ipairs(signs) do
-  vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
+	vim.fn.sign_define(sign.name, { texthl = sign.name, text = sign.text, numhl = "" })
 end
 
 vim.diagnostic.config({
-  virtual_text = false,
-  signs = { active = signs },
-  update_in_insert = true,
-  underline = true,
-  severity_sort = true,
-  float = {
-    focusable = false,
-    style = "minimal",
-    border = "rounded",
-    source = "always",
-    header = "",
-    prefix = "",
-  },
+	virtual_text = false,
+	signs = { active = signs },
+	update_in_insert = true,
+	underline = true,
+	severity_sort = true,
+	float = {
+		focusable = false,
+		style = "minimal",
+		border = "rounded",
+		source = "always",
+		header = "",
+		prefix = "",
+	},
 })
 
 vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(vim.lsp.handlers.hover, { border = "rounded" })
